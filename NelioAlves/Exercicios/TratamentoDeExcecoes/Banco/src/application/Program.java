054da47bc97599ff470011a2f4a74be477f481f9
FowlerAsch <higor.castro@ufms.br>
2024-11-11 17:42:23 -0300

    Atualizando tratamento de Exceções :rocket:

--- NelioAlves/Exercicios/TratamentoDeExcecoes/Banco/src/application/Program.java
+++ NelioAlves/Exercicios/TratamentoDeExcecoes/Banco/src/application/Program.java
@@ -1,10 +1,47 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Conta;
+import exceptions.BusinessException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Informe os dados da conta");
+		System.out.print("Numero: ");
+		int numero = sc.nextInt();
+		
+		System.out.print("Titular: ");
+		sc.nextLine();
+		String titular = sc.nextLine();
+		
+		System.out.print("Saldo inicial: ");
+		double saldo = sc.nextDouble();
+		
+		System.out.print("Limite de saque: ");
+		double limiteDeSaque = sc.nextDouble();
+		
+		Conta conta = new Conta (numero, titular, saldo, limiteDeSaque);
+		
+		System.out.print("Informe uma quantia que deseja sacar: ");
+		double valor = sc.nextDouble();
+		
+		try{
+			conta.sacar(valor);
+			System.out.printf("Novo saldo: %.2f%n", conta.getSaldo());
+		}
+		
+		catch(BusinessException e){
+			System.out.print(e.getMessage());
+		}
+		
+		sc.close();
 	}
 
 }
--- NelioAlves/Exercicios/TratamentoDeExcecoes/Banco/src/entities/Conta.java
+++ NelioAlves/Exercicios/TratamentoDeExcecoes/Banco/src/entities/Conta.java
@@ -1,5 +1,75 @@
 package entities;
 
-public class Conta {
+import exceptions.BusinessException;
 
+public class Conta {
+	
+	private int numero;
+	private String titular;
+	private double saldo;
+	private double LimiteDeSaque;
+	
+	public Conta() {
+		
+	}
+	
+	public Conta(int numero, String titular, double saldo, double LimiteDeSaque) {
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = saldo;
+		this.LimiteDeSaque = LimiteDeSaque;
+	}
+	
+	
+	
+	public int getNumero() {
+		return numero;
+	}
+
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void setLimiteDeSaque(double limiteDeSaque) {
+		LimiteDeSaque = limiteDeSaque;
+	}
+
+
+	public double getLimiteDeSaque() {
+		return LimiteDeSaque;
+	}
+
+	public void deposito(double valor) {
+		saldo += valor;
+	}
+	
+	public void sacar (double valor) {
+		saldo -= valor;
+	}
+	
+	public void validandoSaque(double valor) {
+		if (valor > getLimiteDeSaque()) {
+			throw new BusinessException("Erro de saque: A quantia excede o limite de saque");
+		}
+		if (valor > getSaldo()) {
+			throw new BusinessException("Erro de saque: Saldo insuficiente");
+		}
+	}
 }
